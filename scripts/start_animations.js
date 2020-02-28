caedb3cfe2293356faf337e8b0d301e03e54dc05
Артемий Лысиков <artemy0909@users.noreply.github.com>
2020-02-28 18:26:53 +0700

    animation lag fixed

---
+++ scripts/start_animations.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    addClassToElement("greeting__circles_circle-4", "greeting__circles_circle-4_animation");
+    addClassToElement("greeting__circles_circle-3", "greeting__circles_circle-3_animation");
+    addClassToElement("greeting__circles_circle-2", "greeting__circles_circle-2_animation");
+    addClassToElement("greeting__circles_circle-1", "greeting__circles_circle-1_animation");
+    addClassToElement("menu", "menu_animation");
+    addClassToElement("greeting__container_text_title", "greeting__container_text_title_animation");
+    addClassToElement("greeting__container_text_description", "greeting__container_text_description_animation")
+};
